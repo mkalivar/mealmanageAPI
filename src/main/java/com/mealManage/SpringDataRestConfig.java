39379863c84654ff9a373aa0b7e1281860b7d190
Manoj Kalivarapu <manojkalivarapu@Manojs-MacBook-Pro.local>
2017-11-02 23:12:18 -0700

    Find by Domain Added Ids

--- src/main/java/com/mealManage/SpringDataRestConfig.java
+++ src/main/java/com/mealManage/SpringDataRestConfig.java
@@ -6,8 +6,17 @@ import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
 
+import com.mealManage.model.ClassPupil;
+import com.mealManage.model.Holidays;
+import com.mealManage.model.MealMaster;
+import com.mealManage.model.MealSelect;
+import com.mealManage.model.MealType;
+import com.mealManage.model.School;
 import com.mealManage.model.SchoolClasses;
+import com.mealManage.model.SchoolDistricts;
+import com.mealManage.model.TeacherClassMap;
 import com.mealManage.model.Teachers;
+import com.mealManage.model.Users;
 
 @Configuration
 public class SpringDataRestConfig {
@@ -18,7 +27,14 @@ public class SpringDataRestConfig {
             @Override
             public void configureRepositoryRestConfiguration(
                                  RepositoryRestConfiguration config) {
-                config.exposeIdsFor(Teachers.class, SchoolClasses.class);
+            		config.setDefaultPageSize(Integer.MAX_VALUE);
+                config.exposeIdsFor(ClassPupil.class, Holidays.class,
+                						MealMaster.class, MealSelect.class,
+                						MealType.class, School.class,
+                						SchoolClasses.class,SchoolDistricts.class,
+                						TeacherClassMap.class, Teachers.class,
+                						Users.class
+                						);
             }
         };
 	}
--- src/main/java/com/mealManage/repository/SchoolDistrictRepository.java
+++ src/main/java/com/mealManage/repository/SchoolDistrictRepository.java
@@ -2,6 +2,9 @@ package com.mealManage.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
@@ -12,6 +15,6 @@ import com.mealManage.model.SchoolDistricts;
 // CRUD refers Create, Read, Update, Delete
 
 @RepositoryRestResource(collectionResourceRel = "schoolDistricts", path = "schoolDistricts")
-public interface SchoolDistrictRepository extends PagingAndSortingRepository<SchoolDistricts, String> {
+public interface SchoolDistrictRepository extends PagingAndSortingRepository<SchoolDistricts, Integer> {
 	List<SchoolDistricts> findByCounty(@Param("county") String county);
 }
