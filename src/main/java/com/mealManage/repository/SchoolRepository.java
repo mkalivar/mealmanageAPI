c576570d518d47214a1e81523227d2078a9087dc
Manoj Kalivarapu <manojkalivarapu@Manojs-MacBook-Pro.local>
2017-11-14 15:48:42 -0700

    find onboarded schools

--- src/main/java/com/mealManage/repository/SchoolRepository.java
+++ src/main/java/com/mealManage/repository/SchoolRepository.java
@@ -2,14 +2,19 @@ package com.mealManage.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import com.mealManage.model.MealSelect;
 import com.mealManage.model.School;
 
 @RepositoryRestResource(collectionResourceRel = "school", path = "school")
 public interface SchoolRepository extends PagingAndSortingRepository<School, Integer>{
 	List<School> findBySchoolDistrictId(@Param("schoolDistrictId") Integer schoolDistrictId);
 	List<School> findBySchoolSubDomain(@Param("schoolSubDomain") String schoolSubDomain);
+	
+	@Query("SELECT m FROM School m WHERE m.schoolSubDomain IS NOT NULL")
+	List<School> findOnboardedSchools();
 }
