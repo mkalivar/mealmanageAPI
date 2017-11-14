f310f214fd6f23c6df5c2d84a1561d5a4bd686b4
Manoj Kalivarapu <manojkalivarapu@Manojs-MacBook-Pro.local>
2017-11-14 15:08:30 -0700

    search by Date range for MealSelect

--- src/main/java/com/mealManage/repository/MealSelectRepository.java
+++ src/main/java/com/mealManage/repository/MealSelectRepository.java
@@ -2,10 +2,12 @@ package com.mealManage.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import com.mealManage.model.MealMaster;
 import com.mealManage.model.MealSelect;
 
 @RepositoryRestResource(collectionResourceRel = "mealSelect", path = "mealSelect")
@@ -15,4 +17,7 @@ public interface MealSelectRepository extends PagingAndSortingRepository<MealSel
 	
 	List<MealSelect> findByMealDateAndPupilId(@Param("mealDate") String mealDate,
 			 								 @Param("pupilId") Integer pupilId);
+	
+	@Query("SELECT m FROM MealSelect m WHERE m.pupilId = :pupilId AND m.mealDate BETWEEN :fromDate AND :toDate")
+	List<MealSelect> findByDates(@Param("fromDate") String fromDate, @Param("toDate") String toDate, @Param("pupilId") Integer pupilId);
 }
