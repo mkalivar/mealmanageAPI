5b478a6fa42557aecd91692841ea64ff4ed603fc
Manoj Kalivarapu <manojkalivarapu@Manojs-MacBook-Pro.local>
2017-11-21 23:04:34 -0700

    added search by School for MealSelect View

--- src/main/java/com/mealManage/repository/MealSelectViewRepository.java
+++ src/main/java/com/mealManage/repository/MealSelectViewRepository.java
@@ -1,11 +1,14 @@
 package com.mealManage.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.mealManage.model.MealSelectView;
 
 @RepositoryRestResource(collectionResourceRel = "mealSelectView", path = "mealSelectView")
 public interface MealSelectViewRepository extends PagingAndSortingRepository<MealSelectView, Integer>{
-
+	List<MealSelectView> findBySchoolId(@Param("schoolId") Integer schoolId);
 }
